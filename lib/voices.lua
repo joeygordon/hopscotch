1c36502a0ac096eb625e32fc04682f7feadf6789
Joey Gordon <joey.gordon@gmail.com>
2021-07-23 16:59:02 -0700

    cleanup things

---
+++ lib/voices.lua
@@ -0,0 +1,77 @@
+local sequences = include("lib/sequences")
+local voices = {
+  {
+    sequence = sequences[1], 
+    note = nil,
+    velocity = 60,
+    channel = 1,
+    available = true,
+    length = 10,
+    clock={}
+  },
+  {
+    sequence = sequences[2], 
+    note = nil,
+    velocity = 60,
+    channel = 1,
+    available = true,
+    length = 10,
+    clock={}
+  },
+  {
+    sequence = sequences[3], 
+    note = nil,
+    velocity = 60,
+    channel = 1,
+    available = true,
+    length = 10,
+    clock={}
+  },
+  {
+    sequence = sequences[4], 
+    note = nil,
+    velocity = 60,
+    channel = 1,
+    available = true,
+    length = 10,
+    clock={}
+  },
+  {
+    sequence = sequences[5], 
+    note = nil,
+    velocity = 60,
+    channel = 1,
+    available = true,
+    length = 10,
+    clock={}
+  },
+  {
+    sequence = sequences[6], 
+    note = nil,
+    velocity = 60,
+    channel = 1,
+    available = true,
+    length = 10,
+    clock={}
+  },
+  {
+    sequence = sequences[7], 
+    note = nil,
+    velocity = 60,
+    channel = 1,
+    available = true,
+    length = 10,
+    clock={}
+  },
+  {
+    sequence = sequences[8], 
+    note = nil,
+    velocity = 60,
+    channel = 1,
+    available = true,
+    length = 10,
+    clock={}
+  },
+}
+
+return voices
--- strata.lua
+++ strata.lua
@@ -13,10 +13,11 @@ engine.name = 'PolyPerc'
 music = require 'musicutil'
 m = midi.connect()
 
-local sequences = include("lib/sequences")
+
+local voices = include("lib/voices")
 local screen_y = 40
 local screen_x_mult = 10
-local positions = {
+local voice_status = {
   "_",
   "_",
   "_",
@@ -25,80 +26,6 @@ local positions = {
   "_",
   "_",
   "_",
-}
-local voices = {
-  {
-    sequence = sequences[1], 
-    note = nil,
-    velocity = 60,
-    channel = 1,
-    available = true,
-    length = 10,
-    clock={}
-  },
-  {
-    sequence = sequences[2], 
-    note = nil,
-    velocity = 60,
-    channel = 1,
-    available = true,
-    length = 10,
-    clock={}
-  },
-  {
-    sequence = sequences[3], 
-    note = nil,
-    velocity = 60,
-    channel = 1,
-    available = true,
-    length = 10,
-    clock={}
-  },
-  {
-    sequence = sequences[4], 
-    note = nil,
-    velocity = 60,
-    channel = 1,
-    available = true,
-    length = 10,
-    clock={}
-  },
-  {
-    sequence = sequences[5], 
-    note = nil,
-    velocity = 60,
-    channel = 1,
-    available = true,
-    length = 10,
-    clock={}
-  },
-  {
-    sequence = sequences[6], 
-    note = nil,
-    velocity = 60,
-    channel = 1,
-    available = true,
-    length = 10,
-    clock={}
-  },
-  {
-    sequence = sequences[7], 
-    note = nil,
-    velocity = 60,
-    channel = 1,
-    available = true,
-    length = 10,
-    clock={}
-  },
-  {
-    sequence = sequences[8], 
-    note = nil,
-    velocity = 60,
-    channel = 1,
-    available = true,
-    length = 10,
-    clock={}
-  },
 }
 
 function play_sequence(seq, voice)
@@ -106,11 +33,11 @@ function play_sequence(seq, voice)
     for i=1, #seq do
       clock.sync(1/4)
       if seq[i] == 1 then
-        positions[voice] = "*"
+        voice_status[voice] = "*"
         engine.amp(voices[voice]["velocity"] / 127)
         engine.hz(music.note_num_to_freq(voices[voice]["note"]))
       else
-        positions[voice] = "_"
+        voice_status[voice] = "_"
       end
       redraw()
     end
@@ -135,7 +62,6 @@ end
 -- midi things
 m.event = function(data)
   local d = midi.to_msg(data)
-  print(d)
   if d.type == "note_on" then
     voice_space = find_empty_space()
     voices[voice_space]["note"] = d.note
@@ -150,7 +76,7 @@ m.event = function(data)
         voices[k]["available"] = true
         voices[k]["note"] = nil
         voices[k]["velocity"] = 0
-        positions[k] = "_"
+        voice_status[k] = "_"
         redraw()
       end
     end
@@ -164,7 +90,6 @@ function key(n,z)
       voice_space = find_empty_space()
 
       if voice_space ~= false then
-        print('space', n, voice_space)
         voices[voice_space]["note"] = n
         voices[voice_space]["clock"] = clock.run(play_sequence, voices[voice_space]["sequence"] , voice_space)
       end
@@ -173,7 +98,7 @@ function key(n,z)
         if v["note"] == n then
           clock.cancel(voices[k]["clock"])
           voices[k]["note"] = nil
-          positions[k] = "_"
+          voice_status[k] = "_"
           redraw()
         end
       end
@@ -189,21 +114,21 @@ function redraw()
   -- screen redraw
   screen.clear()
   screen.move(screen_x_mult * 1,screen_y)
-  screen.text(positions[1])
+  screen.text(voice_status[1])
   screen.move(screen_x_mult * 2, screen_y)
-  screen.text(positions[2])
+  screen.text(voice_status[2])
   screen.move(screen_x_mult * 3,screen_y)
-  screen.text(positions[3])
+  screen.text(voice_status[3])
   screen.move(screen_x_mult * 4, screen_y)
-  screen.text(positions[4])
+  screen.text(voice_status[4])
   screen.move(screen_x_mult * 5, screen_y)
-  screen.text(positions[5])
+  screen.text(voice_status[5])
   screen.move(screen_x_mult * 6, screen_y)
-  screen.text(positions[6])
+  screen.text(voice_status[6])
   screen.move(screen_x_mult * 7, screen_y)
-  screen.text(positions[7])
+  screen.text(voice_status[7])
   screen.move(screen_x_mult * 8, screen_y)
-  screen.text(positions[8])
+  screen.text(voice_status[8])
   screen.update()
 end
 
